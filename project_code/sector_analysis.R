039bbd51fa9674689152d6d0c936a4df83ca32a8
danjwalton <20973540+danjwalton@users.noreply.github.com>
2021-07-12 13:10:15 +0100

    Align missing sectors

--- project_code/sector_analysis.R
+++ project_code/sector_analysis.R
@@ -5,9 +5,24 @@ setwd("..")
 
 fts <- fread("project_data/fts_flows.csv", encoding = "UTF-8")
 
+sector.decode <- (fts[!(sector %in% fts$destinationObjects_GlobalCluster.name), .(sector = unique(sector), new_sector = NA_character_)])
+sector.decode[grepl("COVID-19", sector), new_sector := "COVID-19"]
+sector.decode[grepl("Multisector|All non-COVID", sector, ignore.case = T), new_sector := "Multi-sector"]
+sector.decode[grepl("WASH", sector), new_sector := "Water Sanitation Hygiene"]
+sector.decode[grepl("Shelter|NFI", sector), new_sector := "Emergency Shelter and NFI"]
+sector.decode[grepl("Food Security", sector), new_sector := "Food Security"]
+sector.decode[is.na(new_sector), new_sector := "Other"]
+
+fts[sector %in% sector.decode$sector, sector := merge(fts[sector %in% sector.decode$sector, .(sector)], sector.decode, by = "sector")$new_sector]
+
+fts[sector == "", sector := "Unspecified"]
+
 sectors <- fts[, .(amountUSD = sum(amountUSD)), by = .(year, sector)]
 sectors <- sectors[, .(amount = sum(amountUSD)), by = .(year, sector)][order(year, sector)]
 
 sectors[, proportion := amount/sum(amount), by = year]
 
-dcast(sectors, sector ~ year, value.var = c("amount", "proportion"))
+sectors.cast <- dcast(sectors, sector ~ year, value.var = c("amount", "proportion"))
+sectors.cast[is.na(sectors.cast)] <- 0
+
+fwrite(sectors.cast, "output/fts_sectors.csv")
